4f0ac5783cf0fb30c08da2dfbb1096d50eb64e5c
korolslava <sviatoslav.motalo@gmail.com>
2026-03-22 14:12:47 +0200

    feat(events): add custom EventArgs classes

---
+++ GameHub-System/Events/AchievementUnlockedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using GameHub.Models;
+
+namespace GameHub.Events;
+
+public class AchievementUnlockedEventArgs : EventArgs
+{
+    public int UserId { get; set; }
+    public string AchievementCode { get; set; } = string.Empty;
+    public Achievement? Achievement { get; set; }
+    public string Reason { get; set; } = string.Empty;
+    public DateTime Time { get; set; } = DateTime.Now;
+}
---
+++ GameHub-System/Events/SessionEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace GameHub.Events;
+
+public class SessionEventArgs : EventArgs
+{
+    public int UserId { get; set; }
+    public int GameId { get; set; }
+    public DateTime Time { get; set; } = DateTime.Now;
+}
